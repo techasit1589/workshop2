d9a768344696fe8a4002b5be9b81502518e6861c
techasit1589 <techasit.sa@ku.th>
2021-03-16 14:14:18 +0700

    no message

---
+++ string/connatenation.py
@@ -0,0 +1,11 @@
+# case 1
+a = "Hello"
+b = "World"
+c = a + b
+print(c)
+
+# case2
+a = "Hello"
+b = "World"
+c = a + " " + b
+print(c)
---
+++ string/string.py
@@ -0,0 +1,6 @@
+name = "test"
+age = 21
+
+result = f" My name is {name} , and Iam {age}"
+
+print("result", result)
