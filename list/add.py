2e330e8182c424034b97f6ba104a8683bed2df32
techasit1589 <techasit.sa@ku.th>
2021-03-16 14:13:05 +0700

    การใช้งาน list

---
+++ list/add.py
@@ -0,0 +1,10 @@
+# Example 1
+thislist = ["apple", "banana", "cherry"]
+thislist.append("orange")
+
+print(thislist)  # ['apple', 'banana', 'cherry', 'orenge']
+
+# Example 2
+thislist = ["apple", "banana", "cherry"]
+thislist.insert(1, "orange")
+print(thislist)  # ['apple, 'orenge', 'banana', 'cherry']
