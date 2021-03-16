154ce5786d55ab13e7de4317e0b8bf83d73c3de8
techasit1589 <techasit.sa@ku.th>
2021-03-16 14:14:10 +0700

    no message

---
+++ set/remove.py
@@ -0,0 +1,20 @@
+# ex1
+thisset = {"apple", "banana", "cherry"}
+thisset.remove("banana")
+print(thisset)
+
+# ex2
+thisset = {"apple", "banana", "cherry"}
+thisset.discard("banana")
+print(thisset)
+
+# ex3
+thisset = {"apple", "banana", "cherry"}
+thisset.pop()
+print(thisset)  # สุ่มเอาค่าอก
+
+
+# ex4
+thisset = {"apple", "banana", "cherry"}
+thisset.clear()
+print(thisset)  # ค่าว่าง
