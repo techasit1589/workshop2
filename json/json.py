daa1d66e708865787d7e2189fc560e8174137eb3
techasit1589 <techasit.sa@ku.th>
2021-03-16 14:12:54 +0700

    no message

---
+++ json/json.py
@@ -0,0 +1,10 @@
+import json
+
+
+json_string = '{"name": "test & test", "age":21,"city":"Bangkok"}'
+
+python_dict = json.loads(json_string)
+
+print(python_dict["name"])
+print(python_dict["age"])
+print(python_dict["city"])
